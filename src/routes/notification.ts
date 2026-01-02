b9a53aabb27b72a53766eb2d55ca241132d07221
Delight Aberage <delightvincent487@gmail.com>
2026-01-02 13:51:05 +0100

    updated notification route

--- src/routes/notification.ts
+++ src/routes/notification.ts
@@ -3,30 +3,27 @@ import Notification from "../model/notification.js";
 import { UserModel } from "../model/UserScehema.js";
 
 const route = Router();
-
 route.get("/notification/:userId", async (req, res) => {
   const { userId } = req.params;
-
+  console.log(" HIT NOTIFICATION ROUTE for userId:", userId);
 
   try {
+
     const getUser = await UserModel.findById(userId);
 
     if (!getUser) {
-      return res.status(404).json({ message: 'user not found' });
+       res.status(400).json({message: "user with the given Id Does not exist"})
     }
 
     const notifications = await Notification.find({ userId });
 
-
     return res.status(200).json({
       notifications: notifications || [],
     });
-
   } catch (error) {
+    console.error("Notification route error:", error);
     return res.status(500).json({
       message: "Internal server error",
     });
   }
 });
-
-export default route;
