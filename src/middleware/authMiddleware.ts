644bf6e76493424f9ebde6146742ec5a8dd7be4b
Delight Aberage <delightvincent487@gmail.com>
2026-01-08 16:29:49 +0100

    push update

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,20 +1,26 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
+export interface AuthRequest extends Request {
+    user?: any;
+}
 
+export const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization as string | undefined;
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Authorization header missing' });
+    }
 
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ message: 'Token missing' });
+    }
 
-
-export const authMiddleware = (req: Request , res : Response, next : NextFunction) => {
-        const authHeader = req.headers.authorization as string | undefined;
-        if (!authHeader) {
-            return res.status(401).send('Authorization header missing');
-       }
-    
-        const token = authHeader.split(' ')[1];
-        if (!token) {
-            return res.status(401).send('Token missing');
-      }
-
-      next();
-
- }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+}
