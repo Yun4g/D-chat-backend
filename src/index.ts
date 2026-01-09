d525a100281af966873d01a2588fb290b026a132
Delight Aberage <delightvincent487@gmail.com>
2026-01-09 21:23:43 +0100

    updated cors configuration

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ server.use(express.json());
 
 
 server.use(cors({
-  origin: ['http://localhost:5173', 'https://d-chat-frontend.vercel.app'],
+  origin: ['http://localhost:5173', 'https://d-chat-one.vercel.app'],
    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
