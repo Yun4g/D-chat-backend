3c4707a3ec7a8158e28b41993b5c19058892c0c6
Delight Aberage <delightvincent487@gmail.com>
2026-01-17 22:42:03 +0100

    update the bug

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -13,7 +13,7 @@ interface AccessTokenPayload extends JwtPayload {
 
 
 export const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
-    const token = req.cookies?.accessToken;
+    const token = req.cookies?.accesToken;
     if (!token) {
         return res.status(401).json({ message: 'Access Token missing' });
     }
